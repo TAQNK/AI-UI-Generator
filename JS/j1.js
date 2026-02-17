36902d352b89b8349739423eac5b6cea0aaeff0b
Tanishq Pal <147054601+TAQNK@users.noreply.github.com>
2026-02-17 16:34:13 +0530

    Update API endpoint for content generation

--- JS/j1.js
+++ JS/j1.js
@@ -8,7 +8,7 @@ async function generateText() {
 
         const apiKey = 'AIzaSyAcEQCvxfKvtLr47FjE5QYLS2bYlzBDexE'; // Replace with your API key
 
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`, {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=${apiKey}`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
